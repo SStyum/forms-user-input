fead5c05371c46c7271bbef06b6cad58191d93b6
Guilherme Rocha <guirocha1938@gmail.com>
2024-09-10 13:38:08 -0300

    feat: Adds form validation for state approach, on keystroke and on blur.

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -7,6 +7,14 @@ export default function Login() {
     password: '',
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  })
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const passwordIsInvalid = didEdit.password && enteredValues.password !== "";
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -20,6 +28,17 @@ export default function Login() {
     setValues(prevState => ({
       ...prevState,
       [identifier]: value,
+    }));
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false,
+    }))
+  };
+
+  const handleInputBlur = (identifier) => {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true,
     }))
   };
 
@@ -30,12 +49,27 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" value={values.email} onChange={(e) => handleInputChange('email', e.target.value)}/>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={values.email}
+            onBlur={() => handleInputBlur('email')}
+            onChange={(e) => handleInputChange('email', e.target.value)}
+          />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email!</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" value={values.password} onChange={(e) => handleInputChange('password', e.target.value)}/>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={values.password}
+            onBlur={() => handleInputBlur('password')}
+            onChange={(e) => handleInputChange('password', e.target.value)}
+          />
         </div>
       </div>
 
